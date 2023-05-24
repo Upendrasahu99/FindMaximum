b0dc911d4740d3e547c495047107199574b6d1e6
upendra <upendrasahu1199@gmail.com>
2023-05-24 23:48:36 +0530

    Added : Give maximum Integer Value

---
+++ src/main/java/com/bridgelebz/find_max/MaxInt.java
@@ -0,0 +1,24 @@
+package com.bridgelebz.find_max;
+
+public class MaxInt {
+    Integer input1;
+    Integer input2;
+    Integer input3;
+
+    MaxInt(Integer input1, Integer input2, Integer input3) {
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
+    }
+
+    public Integer findMax(Integer input1, Integer input2, Integer input3) {
+        Integer maxValue = input1;
+        if (input2.compareTo(maxValue) > 0) {
+            maxValue = input2;
+        } else if (input3.compareTo(maxValue) > 0) {
+            maxValue = input3;
+        }
+        return maxValue;
+    }
+}
+
